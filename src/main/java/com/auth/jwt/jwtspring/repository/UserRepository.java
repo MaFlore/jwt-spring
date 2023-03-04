a8b1422be29b40f064c2628381ec2f758975b478
AHADJITSE Yawo Florent Mathis <yawo.ahadjitse@ipnetinstitute.com>
2023-03-04 23:41:07 +0000

    Ajout de l'annotation @Repository dans les fichiers du package repository

--- src/main/java/com/auth/jwt/jwtspring/repository/UserRepository.java
+++ src/main/java/com/auth/jwt/jwtspring/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.auth.jwt.jwtspring.repository;
 
 import com.auth.jwt.jwtspring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
